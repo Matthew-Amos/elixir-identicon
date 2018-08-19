c30af3571f59366320dce4e45378131af2624a5a
equinaut <matthewamos@live.com>
2018-08-19 19:45:47 -0400

    mirrow_row and build_grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex_list} = image) do
+    grid =
+      hex_list
+      |> Enum.chunk(3)
+      # Need to pass a reference to mirror row of arity 1
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      # Could've also used Enum.flat_map(%mirrow_row/1)
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second, _third] = row
+
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
